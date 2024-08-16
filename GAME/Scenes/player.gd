60f39a96ee39cee4f75429eaff88afa3e5e2cbb6
Jayguar <128915852+Jayguar@users.noreply.github.com>
2024-08-16 20:28:15 +0100

    big

--- GAME/Scenes/player.gd
+++ GAME/Scenes/player.gd
@@ -13,7 +13,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += 0
+		velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
