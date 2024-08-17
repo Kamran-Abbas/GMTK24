ff02285196172b7981f87e7a6006003f37d56d08
Kamran-Abbas <136983484+Kamran-Abbas@users.noreply.github.com>
2024-08-17 19:19:31 +0100

    made progress on death animation

--- GAME/Scripts/bee.gd
+++ GAME/Scripts/bee.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
-var direction = 1
+var direction = -1
 
 @onready var drct = $AnimatedSprite2D
 
 @onready var rayright = $RayRight
 @onready var rayleft = $RayLeft
+
+func _ready():
+	drct.flip_h = true
 
 func _process(delta):
 	if rayright.is_colliding():
--- GAME/Scripts/killzone.gd
+++ GAME/Scripts/killzone.gd
@@ -1,12 +1,25 @@
 extends Area2D
-@onready var timer = $Timer
+
+@onready var restart_timer = $RestartTimer
+@onready var death_anim_timer = $DeathAnimTimer
+const PLAYER = preload("res://Scenes/player.tscn")
+
+var playerBody
 
 signal player_dead
 
 func _on_body_entered(body):
-	timer.start()
-	body.get_node("CollisionShape2D").queue_free()
+	death_anim_timer.start()
+	restart_timer.start()
+	Engine.time_scale = 0.5
+	playerBody = body
+	body.get_node("AnimatedSprite2D").play("death")
 
 
-func _on_timer_timeout():
+func _on_death_anim_timer_timeout():
+	playerBody.get_node("CollisionShape2D").queue_free()
+
+
+func _on_restart_timer_timeout():
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
